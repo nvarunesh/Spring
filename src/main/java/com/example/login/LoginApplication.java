f0ccbd3af30aaf4ab8e1bd22b1fca1971d6d76ec
nvarunesh <nvarunesh2020@gmail.com>
2026-07-25 21:43:18 +0530

    added shedule related code

--- src/main/java/com/example/login/LoginApplication.java
+++ src/main/java/com/example/login/LoginApplication.java
@@ -2,8 +2,12 @@ package com.example.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.*" })
+@EnableScheduling
+@EnableAsync
 public class LoginApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sheduling/ShedulingSvc.java
@@ -0,0 +1,47 @@
+package com.sheduling;
+
+import java.time.LocalDateTime;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShedulingSvc {
+
+	/**
+	 * This method start every millisecond mentioned in the parameter and it will
+	 * use single thread
+	 */
+	@Scheduled(fixedRate = 5000)
+	public void fixedRate() {
+		System.err.println("Arun--fixedRate--" + LocalDateTime.now().toString());
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	/**
+	 * This method start every millisecond mentioned in the parameter and we added
+	 * the @Async this enable the multiple thread
+	 */
+	@Async
+	@Scheduled(fixedRate = 5000)
+	public void fixedRateWithAynsc() {
+		System.err.println("Arun--fixedRate--" + LocalDateTime.now().toString());
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	@Scheduled(fixedDelay = 5000)
+	public void fixedDelay() {
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("Arun--fixedDelay--" + LocalDateTime.now().toString());
+	}
+}
